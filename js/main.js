7bc7b63ca526781131695cfb0bf9cdb6d8bb79eb
shirin_sultonova <shirin.sultonova@inbox.ru>
2022-08-19 18:19:48 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+var elModalOpenButton = document.querySelector(".menu-btn");
+var elModalOpenlink = document.querySelector(".header-mobile");
+
+elModalOpenButton.addEventListener("click", function(){
+    elModalOpenlink.classList.toggle("nav--active");
+})
